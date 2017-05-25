b4cfa815457aa16db461f1da675a629e78f1e1c0
Kevin Valente <Kevin Valente>
2017-05-25 10:02:45 -0700

    Firebase not defined error

--- assets/Javascript/javascript.js
+++ assets/Javascript/javascript.js
@@ -1,104 +1,101 @@
 	
+
 	//Pseudo Code for making train scheduler
 
 	//create input form with Train name, destination, First time, Frequency X
 	//create Table displaying Train name, Destination, Frequency, next arrival, min away X
 	//create function that uses first train time and frequency to determine next arrival and minutes away
-		//Use Modulo
+	//Use Modulo
 	//
 
 	//use firebase to store form data into table
 
 
 	function createTableEntry(name, destination, time, frequency) {
-		  		
-		  		var row = $("<tr>");
-		  		var nameCol = $("<td>");
-		  		var destinationCol = $("<td>");
-		  		var frequencyCol = $("<td>");
-		  		var nextCol = $("<td>");
-		  		var minAwayCol = $("<td>");
-		  		
-		  		nameCol.text(name);
-		  		destinationCol.text(destination);
-		  		frequencyCol.text(frequency);
-		  		nextCol.text(next);
-		  		minAwayCol.text(min);
 
-		  		row.append(nameCol);
-		  		row.append(destinationCol);
-		  		row.append(frequencyCol);
-		  		row.append(nextCol);
-		  		row.append(minAwayCol);
-		  
-		  		$("#main-table").append(row);
-		  	};
+	    var row = $("<tr>");
+	    var nameCol = $("<td>");
+	    var destinationCol = $("<td>");
+	    var frequencyCol = $("<td>");
+	    var nextCol = $("<td>");
+	    var minAwayCol = $("<td>");
 
-		  	 	function timeCalc(argument) {
+	    nameCol.text(name);
+	    destinationCol.text(destination);
+	    frequencyCol.text(frequency);
+	    nextCol.text(next);
+	    minAwayCol.text(min);
 
-		  	 	var tFrequency = 
+	    row.append(nameCol);
+	    row.append(destinationCol);
+	    row.append(frequencyCol);
+	    row.append(nextCol);
+	    row.append(minAwayCol);
 
-		  	 	// var firstTime = 
+	    $("#main-table").append(row);
+	};
 
-		  	 	// var minutesTilTrain = 
+	// function timeCalc(argument) {
 
-	 // First Time (pushed back 1 year to make sure it comes before current time)
-    			
-    			// var firstTimeConverted = moment(firstTime, "hh:mm").subtract(1, "years");
-    			// console.log(firstTimeConverted);
+	//     var tFrequency =
 
-    //Set Current time
-			    // var currentTime = moment();
-			    // console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"))
+	        // var firstTime = 
 
+	        // var minutesTilTrain = 
 
+	        // First Time (pushed back 1 year to make sure it comes before current time)
+
+	        // var firstTimeConverted = moment(firstTime, "hh:mm").subtract(1, "years");
+	        // console.log(firstTimeConverted);
+
+	        //Set Current time
+	        // var currentTime = moment();
+	        // console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"))
 
 
 
 
 
-		  
 
 
-		  	 database.ref().on("child_added", function(snapshot) {
-			      
-			      name = snapshot.val().name;
-			      destination = snapshot.val().destination;
-			      first = snapshot.val().first;
-			      frequency = snapshot.val().frequency;
-			      JSON.stringify(start);
-			      
-			      createTableEntry(name,destination,first,frequency);
-			    }, function(errorObject) {
-			      console.log("The read failed: " + errorObject.code);
-			    });
+	        var database = furebase.database();
+	        
+			database.ref().on("child_added", function(snapshot) {
+
+                name = snapshot.val().name;
+                destination = snapshot.val().destination;
+                first = snapshot.val().first;
+                frequency = snapshot.val().frequency;
+                JSON.stringify(start);
+
+                createTableEntry(name, destination, first, frequency);
+            }, function(errorObject) {
+                console.log("The read failed: " + errorObject.code);
+            });
 
 
-		  	$("#addChar").on("click", function(e) {
-			    e.preventDefault();
+	      
 
-			});
+	    $("#addChar").on("click", function(e) {
+	        e.preventDefault();
 
-
-		  			$("#addChar").on("click", function(e) {
-			    e.preventDefault();
-
-			    var nameHolder = $("#name-input").val().trim();
-			    var destinationHolder = $("#destition-input").val().trim();
-			    var firstHolder = $("#first-input").val().trim();
-			    var frequencyHolder = $("#frequency-input").val().trim();
-
-			    
-			    database.ref().push({
-			        name: nameHolder,
-			        destination: destinationHolder,
-			        first: firstHolder,
-			        frequency: frequencyHolder
-			      });
-			    
-			});
-
-		 
+	    });
 
 
-		  		
+	    $("#addChar").on("click", function(e) {
+	        e.preventDefault();
+
+	        var nameHolder = $("#name-input").val().trim();
+	        var destinationHolder = $("#destition-input").val().trim();
+	        var firstHolder = $("#first-input").val().trim();
+	        var frequencyHolder = $("#frequency-input").val().trim();
+
+
+	        database.ref().push({
+	            name: nameHolder,
+	            destination: destinationHolder,
+	            first: firstHolder,
+	            frequency: frequencyHolder
+	        });
+
+	    });
