ebe9a69c0049e35e77bddb6cc67c0ec24f04b082
Kevin Valente <Kevin Valente>
2017-05-30 13:33:51 -0700

    added input fields to table/attempted functionality

--- assets/Javascript/javascript.js
+++ assets/Javascript/javascript.js
@@ -1,13 +1,15 @@
- var config = {
-        apiKey: "AIzaSyAAUKZo69Oni3SZ2IHdM4d0D2Z5OkUfIMM",
-        authDomain: "train-scheduler-609dd.firebaseapp.com",
-        databaseURL: "https://train-scheduler-609dd.firebaseio.com",
-        projectId: "train-scheduler-609dd",
-        storageBucket: "train-scheduler-609dd.appspot.com",
-        messagingSenderId: "445767248728"
-    };
-    firebase.initializeApp(config);	
 
+	 // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDddAmd6_SWZAdlM2t7TvsEpzphisXzVm4",
+    authDomain: "train-arrival.firebaseapp.com",
+    databaseURL: "https://train-arrival.firebaseio.com",
+    projectId: "train-arrival",
+    storageBucket: "train-arrival.appspot.com",
+    messagingSenderId: "116901450432"
+  };
+  firebase.initializeApp(config);
+    
     var database = firebase.database();
 
 	//Pseudo Code for making train scheduler
@@ -21,75 +23,64 @@
 	//use firebase to store form data into table
 
 
-	function createTableEntry(name, destination, time, frequency) {
+	function createTableEntry(name, dest, freq, min) {
 
 	    var row = $("<tr>");
 	    var nameCol = $("<td>");
-	    var destinationCol = $("<td>");
-	    var frequencyCol = $("<td>");
+	    var destCol = $("<td>");
+	    var freqCol = $("<td>");
 	    var nextCol = $("<td>");
-	    var minAwayCol = $("<td>");
+	    var minCol = $("<td>");
+	    // var totalCol = $("<td>");
 
 	    nameCol.text(name);
-	    destinationCol.text(destination);
-	    frequencyCol.text(frequency);
-	    nextCol.text(next);
-	    minAwayCol.text(min);
+	    destCol.text(dest);
+	    freqCol.text(freq);
+	    // nextCol.text(next);
+	    minCol.text(min);
 
 	    row.append(nameCol);
-	    row.append(destinationCol);
-	    row.append(frequencyCol);
+	    row.append(destCol);
+	    row.append(freqCol);
 	    row.append(nextCol);
-	    row.append(minAwayCol);
+	    row.append(minCol);
+	    // row.append(totalCol);
 
 	    $("#main-table").append(row);
 	};
 
-	var m = moment();
-	console.log(m);
+	//Function to calculate Arrival time and Minutes Away
+
+		var currentTime = moment().format("HHmm");
+		var t = $("#time-input").val();
+		var arrivalTime = (currentTime - t)%currentTime;
+		var nextArrival = 0;
+		var minLeft = 0;
 
 
+	database.ref().on("child_added", function(snapshot) {
 
+		name = snapshot.val().name;
+		dest = snapshot.val().dest;
+		freq = snapshot.val().freq;
+		min = snapshot.val().min;
+		JSON.stringify(min);
 
+	createTableEntry(name, dest, freq, min);
+});
 
+	$("#addChar").on("click", function(e) {
+		e.preventDefault();
 
+		var nameHolder = $("#name-input").val().trim();
+		var destHolder = $("#dest-input").val().trim();
+		var freqHolder = $("#freq-input").val().trim();
+		var timeHolder = $("#time-input").val().trim();
 
-
-       
-
-		database.ref().on("child_added", function(snapshot) {
-        }, function(errorObject) {
-      });
-
-
-	    $("#addChar").on("click", function(e) {
-	        e.preventDefault();
-
-	        var nameHolder = $("#name-input").val();
-	        var destinationHolder = $("#destination-input").val();
-	        var firstHolder = $("#first-input").val();
-	        var frequencyHolder = $("#frequency-input").val();
-
-	         firebase.database().ref().on("value", function(snapshot){
-			$("#name-input").html(snapshot.val().nameHolder);
-			$("#destination-input").html(snapshot.val().destinationHolder);
-			$("#first-input").html(snapshot.val().firstHolder);
-			$("#frequency-input").html(snapshot.val().frequencyHolder);
-
-
-
-	        database.ref().push({
-	            name: nameHolder,
-	            destination: destinationHolder,
-	            first: firstHolder,
-	            frequency: frequencyHolder
-	        });
-
-	        $('#theForm')[0].reset();
-
-	       
-
+	database.ref().push({
+		name: nameHolder,
+		dest: destHolder,
+		freq: freqHolder,
+		time: timeHolder,
 		});
-
-
-	 });
+	});
